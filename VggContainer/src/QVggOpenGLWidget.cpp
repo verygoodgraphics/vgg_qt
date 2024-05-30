b0a0ec8528d0e57e2c8733bde11e7db075f038eb
Guanhua Hou <gaimanjing@gmail.com>
2024-05-30 15:16:26 +0800

    format

--- VggContainer/src/QVggOpenGLWidget.cpp
+++ VggContainer/src/QVggOpenGLWidget.cpp
@@ -28,27 +28,34 @@
 // ======================================================================
 // QVggOpenGLWidgetImpl
 // ======================================================================
-class QVggOpenGLWidgetImpl {
+class QVggOpenGLWidgetImpl
+{
   friend QVggOpenGLWidget;
 
-  QVggOpenGLWidget *m_api;
+  QVggOpenGLWidget*                 m_api;
   std::unique_ptr<VGG::QtContainer> m_container;
 
   QOpenGLFunctions m_funcs;
 
-  QTimer m_animator;
+  QTimer  m_animator;
   QPointF m_lastMouseMovePosition;
 
 public:
-  QVggOpenGLWidgetImpl(QVggOpenGLWidget *api)
-      : m_api{api}, m_container{new VGG::QtContainer} {
+  QVggOpenGLWidgetImpl(QVggOpenGLWidget* api)
+    : m_api{ api }
+    , m_container{ new VGG::QtContainer }
+  {
     m_animator.setInterval(16);
   }
 
   // === GL ============================================================
-  void initializeGL() { m_funcs.initializeOpenGLFunctions(); }
+  void initializeGL()
+  {
+    m_funcs.initializeOpenGLFunctions();
+  }
 
-  void resizeGL(int w, int h) {
+  void resizeGL(int w, int h)
+  {
     auto scale = m_api->windowHandle()->devicePixelRatio();
 
     UEvent evt;
@@ -62,37 +69,45 @@ public:
     m_container->onEvent(evt);
   }
 
-  void paintGL() { m_container->paint(true); }
+  void paintGL()
+  {
+    m_container->paint(true);
+  }
 
   // =================================================================
-  void init(int w, int h) {
+  void init(int w, int h)
+  {
     m_funcs.glViewport(0, 0, w, h);
     m_container->init(w, h, m_api->windowHandle()->devicePixelRatio());
   }
 
   // === api =========================================================
-  bool load(const std::string &filePath,
-            const char *designDocSchemaFilePath = nullptr,
-            const char *layoutDocSchemaFilePath = nullptr) {
-    return m_container->load(filePath, designDocSchemaFilePath,
-                             layoutDocSchemaFilePath);
+  bool load(
+    const std::string& filePath,
+    const char*        designDocSchemaFilePath = nullptr,
+    const char*        layoutDocSchemaFilePath = nullptr)
+  {
+    return m_container->load(filePath, designDocSchemaFilePath, layoutDocSchemaFilePath);
   }
 
-  void setEventListener(QVggOpenGLWidget::EventListener listener) {
-    if (listener) {
+  void setEventListener(QVggOpenGLWidget::EventListener listener)
+  {
+    if (listener)
+    {
       auto sdk = m_container->sdk();
-      m_container->setEventListener([listener, sdk](std::string type,
-                                                    std::string targetId,
-                                                    std::string targetPath) {
-        listener(sdk, type, targetId, targetPath);
-      });
-    } else {
+      m_container->setEventListener(
+        [listener, sdk](std::string type, std::string targetId, std::string targetPath)
+        { listener(sdk, type, targetId, targetPath); });
+    }
+    else
+    {
       m_container->setEventListener(nullptr);
     }
   }
 
   // === events =====================================================
-  void mousePressEvent(QMouseEvent *event) {
+  void mousePressEvent(QMouseEvent* event)
+  {
     UEvent evt;
     evt.button.type = VGG_MOUSEBUTTONDOWN;
     fillVggEvent(evt, event);
@@ -100,7 +115,8 @@ public:
     m_container->onEvent(evt);
   }
 
-  void mouseMoveEvent(QMouseEvent *event) {
+  void mouseMoveEvent(QMouseEvent* event)
+  {
     UEvent evt;
     evt.motion.type = VGG_MOUSEMOTION;
     evt.motion.windowX = event->position().x();
@@ -115,7 +131,8 @@ public:
     m_lastMouseMovePosition = event->position();
   }
 
-  void mouseReleaseEvent(QMouseEvent *event) {
+  void mouseReleaseEvent(QMouseEvent* event)
+  {
     UEvent evt;
     evt.button.type = VGG_MOUSEBUTTONUP;
     fillVggEvent(evt, event);
@@ -123,7 +140,8 @@ public:
     m_container->onEvent(evt);
   }
 
-  void wheelEvent(QWheelEvent *event) {
+  void wheelEvent(QWheelEvent* event)
+  {
     UEvent evt;
     evt.wheel.type = VGG_MOUSEWHEEL;
 
@@ -140,33 +158,37 @@ public:
     m_container->onEvent(evt);
   }
 
-  void keyPressEvent(QKeyEvent *event) {
+  void keyPressEvent(QKeyEvent* event)
+  {
     auto vggEvent = QVggEventAdapter::keyPressEvent(event);
     m_container->onEvent(vggEvent);
   }
 
-  void keyReleaseEvent(QKeyEvent *event) {
+  void keyReleaseEvent(QKeyEvent* event)
+  {
     auto vggEvent = QVggEventAdapter::keyReleaseEvent(event);
     m_container->onEvent(vggEvent);
   }
 
   // === Private ===========================================================
 private:
-  void fillVggEvent(UEvent &vggEvent, QMouseEvent *mouseEvent) {
-    switch (mouseEvent->button()) {
-    case Qt::LeftButton:
-      // jsButtonIndex + 1
-      // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button#value
-      vggEvent.button.button = 1;
-      break;
-    case Qt::MiddleButton:
-      vggEvent.button.button = 2;
-      break;
-    case Qt::RightButton:
-      vggEvent.button.button = 3;
-      break;
-    default:
-      break;
+  void fillVggEvent(UEvent& vggEvent, QMouseEvent* mouseEvent)
+  {
+    switch (mouseEvent->button())
+    {
+      case Qt::LeftButton:
+        // jsButtonIndex + 1
+        // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button#value
+        vggEvent.button.button = 1;
+        break;
+      case Qt::MiddleButton:
+        vggEvent.button.button = 2;
+        break;
+      case Qt::RightButton:
+        vggEvent.button.button = 3;
+        break;
+      default:
+        break;
     }
 
     auto p = mouseEvent->position();
@@ -178,52 +200,67 @@ private:
 // ======================================================================
 // QVggOpenGLWidget
 // ======================================================================
-QVggOpenGLWidget::QVggOpenGLWidget(QWidget *parent)
-    : QOpenGLWidget(parent), m_impl(new QVggOpenGLWidgetImpl(this)) {
-  QObject::connect(&m_impl->m_animator, &QTimer::timeout, this, [this]() {
-    if (this->m_impl->m_container->needsPaint()) {
-      this->update();
-    }
-    this->m_impl->m_container->dispatch(); // todo, improve dispatch
-  });
+QVggOpenGLWidget::QVggOpenGLWidget(QWidget* parent)
+  : QOpenGLWidget(parent)
+  , m_impl(new QVggOpenGLWidgetImpl(this))
+{
+  QObject::connect(
+    &m_impl->m_animator,
+    &QTimer::timeout,
+    this,
+    [this]()
+    {
+      if (this->m_impl->m_container->needsPaint())
+      {
+        this->update();
+      }
+      this->m_impl->m_container->dispatch(); // todo, improve dispatch
+    });
   m_impl->m_animator.start();
 
   setMouseTracking(true);
 }
 
-QVggOpenGLWidget::~QVggOpenGLWidget() {
+QVggOpenGLWidget::~QVggOpenGLWidget()
+{
   m_impl->m_animator.stop();
   delete m_impl;
 }
 
 // === api ===============================================================
-bool QVggOpenGLWidget::load(const std::string &filePath,
-                            const char *designDocSchemaFilePath,
-                            const char *layoutDocSchemaFilePath) {
-  return m_impl->load(filePath, designDocSchemaFilePath,
-                      layoutDocSchemaFilePath);
+bool QVggOpenGLWidget::load(
+  const std::string& filePath,
+  const char*        designDocSchemaFilePath,
+  const char*        layoutDocSchemaFilePath)
+{
+  return m_impl->load(filePath, designDocSchemaFilePath, layoutDocSchemaFilePath);
 }
 
-void QVggOpenGLWidget::setEventListener(EventListener listener) {
+void QVggOpenGLWidget::setEventListener(EventListener listener)
+{
   m_impl->setEventListener(listener);
 }
 
 // === GL ===============================================================
-void QVggOpenGLWidget::initializeGL() {
+void QVggOpenGLWidget::initializeGL()
+{
   m_impl->initializeGL();
   init(this->width(), this->height());
 
   context()->setShareContext(QOpenGLContext::globalShareContext());
 }
 
-void QVggOpenGLWidget::resizeGL(int w, int h) {
+void QVggOpenGLWidget::resizeGL(int w, int h)
+{
   init(w, h);
 
   m_impl->resizeGL(w, h);
 }
 
-void QVggOpenGLWidget::paintGL() {
-  if (!this->isVisible()) {
+void QVggOpenGLWidget::paintGL()
+{
+  if (!this->isVisible())
+  {
     return;
   }
 
@@ -231,27 +268,36 @@ void QVggOpenGLWidget::paintGL() {
 }
 
 // === events ===============================================================
-void QVggOpenGLWidget::mousePressEvent(QMouseEvent *event) {
+void QVggOpenGLWidget::mousePressEvent(QMouseEvent* event)
+{
   m_impl->mousePressEvent(event);
 }
-void QVggOpenGLWidget::mouseReleaseEvent(QMouseEvent *event) {
+void QVggOpenGLWidget::mouseReleaseEvent(QMouseEvent* event)
+{
   m_impl->mouseReleaseEvent(event);
 }
 
-void QVggOpenGLWidget::mouseMoveEvent(QMouseEvent *event) {
+void QVggOpenGLWidget::mouseMoveEvent(QMouseEvent* event)
+{
   m_impl->mouseMoveEvent(event);
 }
 
-void QVggOpenGLWidget::wheelEvent(QWheelEvent *event) {
+void QVggOpenGLWidget::wheelEvent(QWheelEvent* event)
+{
   m_impl->wheelEvent(event);
 }
 
-void QVggOpenGLWidget::keyPressEvent(QKeyEvent *event) {
+void QVggOpenGLWidget::keyPressEvent(QKeyEvent* event)
+{
   m_impl->keyPressEvent(event);
 }
-void QVggOpenGLWidget::keyReleaseEvent(QKeyEvent *event) {
+void QVggOpenGLWidget::keyReleaseEvent(QKeyEvent* event)
+{
   m_impl->keyReleaseEvent(event);
 }
 
 // === private ===============================================================
-void QVggOpenGLWidget::init(int w, int h) { m_impl->init(w, h); }
+void QVggOpenGLWidget::init(int w, int h)
+{
+  m_impl->init(w, h);
+}
