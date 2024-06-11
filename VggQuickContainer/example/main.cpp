a5b07d986305ff07b667e648f5d058857197dbd9
szn <80252484@qq.com>
2024-06-11 10:12:19 +0800

    Basic support for the rendering and interaction of Daruma files.

--- VggQuickContainer/example/main.cpp
+++ VggQuickContainer/example/main.cpp
@@ -1,23 +1,44 @@
+#include "VGG/Environment.hpp"
+#include "QVggEventAdapter.hpp"
+#include "QVggQuickItem.h"
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QtQml/qqmlextensionplugin.h>
+#include <QQuickWindow>
 
-Q_IMPORT_QML_PLUGIN(VggQuickContainerPlugin)
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QGuiApplication app(argc, argv);
+  VGG::Environment::setUp();
+  QVggEventAdapter::setup();
+
+  QGuiApplication app(argc, argv);
+  qmlRegisterType<QVggQuickItem>("QVggQuickItem", 1, 0, "QVggQuickItem");
+
+  // qt6 need this
+  // QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
+
+  QObject::connect(&app, &QGuiApplication::aboutToQuit, [&]() { VGG::Environment::tearDown(); });
+
+  int returnValue = 0;
+
+  {
 
     QQmlApplicationEngine engine;
-    // The first subfolder is the libraryName followed by the regular
-    // folder structure: LibraryName/Subfolder
-    const QUrl url(u"qrc:/VggQtQuickDemoApp/example/main.qml"_qs);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
+    const QUrl            url("qrc:/main.qml");
+    QObject::connect(
+      &engine,
+      &QQmlApplicationEngine::objectCreated,
+      &app,
+      [url](QObject* obj, const QUrl& objUrl)
+      {
         if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
+          QCoreApplication::exit(-1);
+      },
+      Qt::QueuedConnection);
     engine.load(url);
 
-    return app.exec();
+    returnValue = app.exec();
+  }
+
+  QVggQuickItem::tearDown();
+  return returnValue;
 }
